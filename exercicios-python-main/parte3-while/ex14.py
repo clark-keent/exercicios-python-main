64307d8d947c564d36893c71fa46384970e53a74
clark-keent <lucas_schissi@estudante.sesisenai.org.br>
2026-09-23 18:22:02 -0300

    exercicio 14 realizado

---
+++ exercicios-python-main/parte3-while/ex14.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um numero que eu direi toda a tabuada dele: "))
+contador = 1
+
+while contador <= 1:
+    resultado = numero * contador
+    print(f"{numero} x {contador} = {resultado}")
+    contador = contador + 1
+    
+
